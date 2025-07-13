b1e5bfeca00e9344f3f0954faec5ae27ce0a60c3
Bryan Mino <bryanmt496@gmail.com>
2025-07-13 01:51:16 -0500

    Adding changes

---
+++ scripts/getdates.js
@@ -0,0 +1,16 @@
+// Code for the current year
+const currentyear = document.querySelector("#currentyear");
+
+const today = new Date();
+
+currentyear.textContent = today.getFullYear();
+
+// Code for modification date
+let lastmodifiedtag = document.querySelector("#lastModified");
+
+const lastmodifieddate = document.lastModified;
+
+lastmodifiedtag.textContent = `Last modification: ${lastmodifieddate}`;
+
+
+
